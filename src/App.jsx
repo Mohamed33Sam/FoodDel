2c82d6bccf1ebdeab27d4b68d18e03edcc3319d0
Mohamed33Sam <mohamed3sam69@gmail.com>
2024-08-08 01:04:33 +0300

    one

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import NavBar from "./components/NavBar/NavBar"
+import Home from "./Pages/Home/Home"
+import Cart from "./Pages/Cart/Cart"
+import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder"
+
+const App = () => {
+  return (
+    <div className="app">
+
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Order" element={<PlaceOrder />} />
+
+
+      </Routes>
+    </div>
+  )
+}
+
+export default App
+
+
